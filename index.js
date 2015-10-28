b03750ced207dedd77bc7a409cda1295e98cac0b
nennad <nennadgal@gmail.com>
2015-10-28 12:19:41 +0000

    function prototypes

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 var async = require('async');
 var Compare = require('./lib/compare.js');
 var Execute = require('./lib/execute.js');
+var mysql = require('mysql');
 
-function MysqlTransit(mysqlOrigin, mysqlDestination, next) {
-  var compare = new Compare(mysqlOrigin, mysqlDestination);
-  var execute = new Execute();
+function MysqlTransit(origDB, tempDB, config) {
+  this.compare = new Compare(origDB, tempDB);
+  this.execute = new Execute(origDB);
+  this.connection = null;
+  this.mysqlConfig = config;
+
+  this._init();
+};
+
+MysqlTransit.prototype._init = function() {
+  this.connection = mysql.createConnection(config);
+};
+
+MysqlTransit.prototype.transit = function(opts, callback) {
+  var self = this;
   async.waterfall([
-      createMysqlConnection(),
-      compare.getAllTablesInTempDatabase(),
-      compare.generateSQLMigration(),
-      execute.runMigration(),
-    ],
-    function(err, result) {
-      return next();
-    });
+    self.compare(callback),
+    this.execute(queries, callback)
+  ], function(err, result) {
+    if (err) return callback(err);
+
+    callback(null, result);
+  });
 }
 
 module.exports = MysqlTransit;
