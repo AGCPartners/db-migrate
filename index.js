d92a283d48368dccddb2bbaddfd66df22b406c5c
darko <darko.krmpotic@agcpartners.co.uk>
2016-05-13 09:14:52 +0200

    index (primary, unique, fulltext) migration

--- index.js
+++ index.js
@@ -270,8 +270,6 @@ MysqlTransit.prototype.transit = function(opt, next) {
                   }
                 });
 
-                // console.log(self.addedFields);
-
                 var verify = removedFields.map(function(rmField) {
                   return function(cb) {
                     if (self.interactive === true) {
@@ -375,29 +373,173 @@ MysqlTransit.prototype.transit = function(opt, next) {
         });
         return callback();
       },
+      function generateSQLMigrationKeys(callback) {
+        self.compareKeysQueries = self.arrTable.map(function(table) {
+          return function(callback) {
+            self.connection.query(util.format(queries.COMPARE_KEYS, self.dbTemp, self.dbTemp, self.dbOriginal, table),
+              function(err, results) {
+                if (err) return callback(err);
+
+                var singleTableQueries = [];
+                var addedKeys = [];
+                var removedKeys = [];
+                var modifiedKeys = [];
+
+                results.forEach(function(res) {
+                  if( res.action === 'ADD') {
+                    addedKeys.push({ name: res.index_name, type: res.index_type, non_unique: res.non_unique , columns: res.columns});
+                  } else if ( res.action === 'DROP') {
+                    removedKeys.push({ name: res.index_name, type: res.index_type, non_unique: res.non_unique , columns: res.columns});
+                  } else if ( res.action === 'MODIFY') {
+                    modifiedKeys.push({ name: res.index_name, type: res.index_type, non_unique: res.non_unique , columns: res.columns});
+                  }
+                });
+             
+                var verify = removedKeys.map(function(rmIndex) {
+                  return function(cb) {
+                    if (self.interactive === true) {
+                      var possibleAnswers = [];
+                        possibleAnswers.unshift({
+                          name: 'Removed',
+                          value: 0
+                        },
+                        {
+                          name: 'Skipped',
+                          value: 1
+                        });
+
+                      inq.prompt([
+                        {
+                          name: 'verify',
+                          type: 'list',
+                          message: 'In ' + table + ' table the index `' + rmIndex.name + '` should be:',
+                          choices: possibleAnswers
+                        }
+                      ], function(answer) {
+                        answer = parseInt(answer.verify);
+                        switch (answer) {
+                          case 0:
+                          {
+                            if(rmIndex.name === 'PRIMARY'){
+                              //DROP primary KEY
+                              singleTableQueries.push(util.format(queries.DROP_PRIMARY, table));  
+                            } else {
+                              //DROP index
+                              singleTableQueries.push(util.format(queries.DROP_INDEX, table, rmIndex.name));
+                            }
+                            cb();
+                            break;
+                          }
+                          case 1:
+                          {
+                            cb();
+                            break;
+                          }
+                        }
+                      });
+                    } else {
+                      cb();
+                    }
+                  }
+                });
+
+                async.series(verify, function(err, result) {
+                  if (err) return callback(err);
+
+                  addedKeys.forEach(function(newIndex) {
+                    if( newIndex.name === 'PRIMARY'){
+                      singleTableQueries.push(util.format(queries.ADD_PRIMARY, table, newIndex.columns));
+                    } else {
+                      var indexType = '';
+                      if(newIndex.type === 'FULLTEXT'){
+                        indexType = 'FULLTEXT';
+                      }else if(newIndex.type === 'BTREE' && newIndex.non_unique == 0){
+                        indexType = 'UNIQUE';
+                      }
+                      singleTableQueries.push(util.format(queries.ADD_INDEX, table, indexType, newIndex.name, newIndex.columns));
+                    }
+                    
+                  });
+
+                  modifiedKeys.forEach(function(modifiedKey) {
+                    if( modifiedKey.name === 'PRIMARY'){
+                      singleTableQueries.push(util.format(queries.MODIFY_PRIMARY, table, modifiedKey.columns))  
+                    } else {
+                      var indexType = '';
+                      if(modifiedKey.type === 'FULLTEXT'){
+                        indexType = 'FULLTEXT';
+                      }else if(modifiedKey.type === 'BTREE' && modifiedKey.non_unique == 0){
+                        indexType = 'UNIQUE';
+                      }
+                      singleTableQueries.push(util.format(queries.MODIFY_INDEX, table, modifiedKey.name, indexType, modifiedKey.name, modifiedKey.columns))  
+                    }
+                    
+                  });
+
+                  singleTableQueries.forEach(function(query) {
+                    self.queryQueue.push(function(cb) {
+                      if (self.interactive === true) {
+                        inq.prompt([{
+                          name: 'exec',
+                          message: 'Query: ' + query + ' Execute?',
+                          type: 'confirm',
+                          default: true
+                        }], function(answer) {
+                          if (answer.exec) {
+                            self.executeQuery(query, function(err) {
+                              if (err) return cb(err);
+
+                              cb();
+                            });
+                          } else {
+                            console.log('Skipped');
+                            cb();
+                          }
+                        });
+                      } else {
+                        self.executeQuery(query, function(err) {
+                          if (err) return cb(err);
+
+                          cb();
+                        });
+                      }
+                    });
+                  });
+                  callback();
+                });
+              });
+          };
+        });
+        return callback();
+      },
       function run(callback) {
         async.series(self.compareQueries, function(err, results) {
           if (err) return callback(err);
 
-          // here we switch to the original db
-          self.connection.query(util.format(queries.SWITCH_DB, self.dbOriginal), function() {
+          async.series( self.compareKeysQueries,function(err,results) {
             if (err) return callback(err);
 
-            // here we execute migration queries one by one
-            async.series(self.queryQueue, function(err, result) {
-              if (err) return callback(err);
+            // here we switch to the original db
+            self.connection.query(util.format(queries.SWITCH_DB, self.dbOriginal), function() {
+              
 
-              if (self.interactive === true) {
-                console.log('Done.');
-              }
-              // remove temporary database
-              self.connection.query(util.format(queries.SWITCH_DB, self.dbOriginal), function() {
-                self.connection.query(util.format(queries.DROP_DATABASE, self.dbTemp),function(){
-                  if (self.interactive === true) {
-                    console.log('Temporary database ' + self.dbTemp + ' was removed.');
-                  }
-                  return callback();
+              // here we execute migration queries one by one
+              async.series(self.queryQueue, function(err, result) {
+                if (err) return callback(err);
+
+                if (self.interactive === true) {
+                  console.log('Done.');
+                }
+                // remove temporary database
+                self.connection.query(util.format(queries.SWITCH_DB, self.dbOriginal), function() {
+                  self.connection.query(util.format(queries.DROP_DATABASE, self.dbTemp),function(){
+                    if (self.interactive === true) {
+                      console.log('Temporary database ' + self.dbTemp + ' was removed.');
+                    }
+                    return callback();
+                  });
                 });
+                
               });
             });
           });
